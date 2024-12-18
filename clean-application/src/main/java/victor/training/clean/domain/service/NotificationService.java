0829a03d456be334ac1206afb27d2e6874027723
rafi.isaharov <rafi.isaharov@dh.com>
2024-12-18 21:02:51 +0200

    extracted infrastructure out of core logic

--- clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
@@ -6,11 +6,6 @@ import org.springframework.stereotype.Service;
 import victor.training.clean.domain.model.Customer;
 import victor.training.clean.domain.model.Email;
 import victor.training.clean.infra.EmailSender;
-import victor.training.clean.infra.LdapApi;
-import victor.training.clean.infra.LdapUserDto;
-
-import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -76,30 +71,4 @@ public class NotificationService {
     emailSender.sendEmail(email);
   }
 
-}
-//So you create the structure you map in front of you to the structure so that it's all clear,
-// and then you start it as a method and then as a separate class.
-// Of course the class will not stay here once it's done, and it compiles, you need to move class to a different place.
-@RequiredArgsConstructor
-@Slf4j
-@Service
-class LdapUserService {
-  private final LdapApi ldapApi;
-
-  public User fetchUser(String usernamePart) {
-    LdapUserDto ldapUserDto = fetchUserFromLdap(usernamePart);
-    if (ldapUserDto.getUn().startsWith("s")) {
-      ldapUserDto.setUn("system"); // ⚠️ dirty hack: replace any system user with 'system'
-    }
-    return new User(ldapUserDto.getUn(), Optional.ofNullable(ldapUserDto.getWorkEmail()), ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase());
-  }
-
-  private LdapUserDto fetchUserFromLdap(String usernamePart) {
-    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(usernamePart.toUpperCase(), null, null);
-
-    if (dtoList.size() != 1) {
-      throw new IllegalArgumentException("Search for username='" + usernamePart + "' did not return a single result: " + dtoList);
-    }
-    return dtoList.get(0);
-  }
 }
