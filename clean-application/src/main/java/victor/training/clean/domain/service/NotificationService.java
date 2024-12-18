b70b8c10d2d35445430923b913ad07e20e19954f
rafi.isaharov <rafi.isaharov@dh.com>
2024-12-18 20:23:48 +0200

    created a new user that wrap external ldap dto dirty class to well known new user object to me video: part2: 2:05:00

--- clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
@@ -10,6 +10,7 @@ import victor.training.clean.infra.LdapApi;
 import victor.training.clean.infra.LdapUserDto;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -20,34 +21,43 @@ public class NotificationService {
 
   // Core application logic, my Zen garden 🧘☯☮️
   public void sendWelcomeEmail(Customer customer, String usernamePart) {
-    // ⚠️ Scary, large external DTO TODO extract needed parts into a new dedicated Value Object
-    LdapUserDto ldapUserDto = fetchUserFromLdap(usernamePart);
-
+    //I don't want to call it get user, it's too vague.
+//    It's too weak.very insignificant.
+//    So fetch, retrieve, resolve something powerful
+//    So I took the garbage out of my method.
+//    get = in memory stuff,
+//    fetch = from the outside API
+//    find = from the database
+    User user = fetchUser(usernamePart);
     // ⚠️ Data mapping mixed with core logic TODO pull it earlier
-    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
 
     Email email = Email.builder()
         .from("noreply@cleanapp.com")
         .to(customer.getEmail())
         .subject("Welcome!")
-        .body("Dear " + customer.getName() + ", welcome! Sincerely, " + fullName)
+        .body("Dear " + customer.getName() + ", welcome! Sincerely, " + user.fullName())
         .build();
 
 
     // ⚠️ Unguarded nullable fields can cause NPE in other places TODO return Optional<> from getter
-    if (ldapUserDto.getWorkEmail() != null) {
+    if (user.email().isPresent()) {
       // ⚠️ Logic repeated in other places TODO move logic to the new class
-      String contact = fullName + " <" + ldapUserDto.getWorkEmail().toLowerCase() + ">";
+      String contact = user.fullName() + " <" + user.email().get().toLowerCase() + ">";
       email.getCc().add(contact);
     }
 
     emailSender.sendEmail(email);
 
     // ⚠️ Swap this line with next one to cause a bug (=TEMPORAL COUPLING) TODO make immutable💚
-    normalize(ldapUserDto);
 
     // ⚠️ 'un' = bad name TODO in my ubiquitous language 'un' means 'username'
-    customer.setCreatedByUsername(ldapUserDto.getUn());
+    customer.setCreatedByUsername(user.username());
+  }
+
+  private User fetchUser(String usernamePart) {
+    LdapUserDto ldapUserDto = fetchUserFromLdap(usernamePart);
+    normalize(ldapUserDto);
+    return new User(ldapUserDto.getUn(), Optional.ofNullable(ldapUserDto.getWorkEmail()), ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase());
   }
 
   private LdapUserDto fetchUserFromLdap(String usernamePart) {
@@ -78,8 +88,12 @@ public class NotificationService {
         .body(returnOrdersStr +
               "Yours sincerely, " + userLdapDto.getFname() + " " + userLdapDto.getLname().toUpperCase())
         .build();
-
+//Secondly, the same kind of logic appears somewhere else in this file.
+//It's very similar. It's over here
     String contact = userLdapDto.getFname() + " " + userLdapDto.getLname().toUpperCase()
+            //    For example, here I'm running the risk of hitting one point or exception
+            //    because the work e-mail is missing in the detail. coming from the outside.
+            //    there is potentially still a null pointer exception waiting to happen
                + " <" + userLdapDto.getWorkEmail().toLowerCase() + ">";
     email.getCc().add(contact);
 
